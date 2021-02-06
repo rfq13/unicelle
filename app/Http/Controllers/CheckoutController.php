1c54e4942c7ad9d39ddd826a5da98560c2f1a212
rfq <hadimrifqyfakhrul@gmail.com>
2021-02-06 08:34:46 +0700

    up

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -137,7 +137,7 @@ class CheckoutController extends Controller
 
     public function get_shipping_info(Request $request)
     {
-        $carts = Auth::check() ? \App\Models\Cart::where("user_id",Auth::user()->id)->with("product")->get() : $carts; 
+        $carts = Auth::check() ? \App\Models\Cart::where("user_id",Auth::user()->id)->with("product")->get() : []; 
 
         if($carts && $carts->count() > 0){
             $categories = Category::all();
@@ -150,7 +150,7 @@ class CheckoutController extends Controller
                         $qty = $product->stocks->sum('qty');
                     }
 
-                    if($qty <= 0) flash("$product->name kosong")->warning(); return back();
+                    // if($qty <= 0) flash("$product->name kosong")->warning(); return back();
                 }
             }
 
